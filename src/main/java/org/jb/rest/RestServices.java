e4382408684dc0ba0e85fe044a14000e205d31a1
jobinbasani <jobinbasani@gmail.com>
2016-07-17 12:59:51 -0300

    Add JSON support

--- src/main/java/org/jb/rest/RestServices.java
+++ src/main/java/org/jb/rest/RestServices.java
@@ -2,14 +2,21 @@ package org.jb.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jb.vo.FamilyInfo;
 
 @Path("service")
+@Produces(MediaType.APPLICATION_JSON)
 public class RestServices extends JaxRsActivator {
 	
 	@GET
 	@Path("names")
-	public String printNames(){
-		return "jobin";
+	public FamilyInfo printNames(){
+		FamilyInfo family = new FamilyInfo();
+		family.setAddress("38 Sienna Crt, Timberlea");
+		return family;
 	}
 
 }
