2929bbc37d713543329c45c3387f1b875ff6f323
jobinbasani <jobinbasani@gmail.com>
2016-07-26 20:27:23 -0300

    Code cleanup

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -2,9 +2,12 @@ package util;
 
 public class Constants {
 
-	private Constants() {}
-	
-	public static final String OPENSHIFT_POSTGRESQL_DB_HOST = "OPENSHIFT_POSTGRESQL_DB_HOST";
-	public static final String OPENSHIFT_POSTGRESQL_DB_PORT = "OPENSHIFT_POSTGRESQL_DB_PORT";
+    private Constants() {
+    }
+
+    public static final String OPENSHIFT_POSTGRESQL_DB_HOST = "OPENSHIFT_POSTGRESQL_DB_HOST";
+    public static final String OPENSHIFT_POSTGRESQL_DB_PORT = "OPENSHIFT_POSTGRESQL_DB_PORT";
+    public static final String DEFAULT_DB_HOST = "localhost";
+    public static final String DEFAULT_DB_PORT = "5432";
 
 }
--- src/test/java/org/jb/test/services/BaseTestCase.java
+++ src/test/java/org/jb/test/services/BaseTestCase.java
@@ -9,48 +9,47 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static util.Constants.OPENSHIFT_POSTGRESQL_DB_HOST;
-import static util.Constants.OPENSHIFT_POSTGRESQL_DB_PORT;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static util.Constants.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=AppConfig.class)
+@ContextConfiguration(classes = AppConfig.class)
 @WebAppConfiguration
 public abstract class BaseTestCase {
 
-	public BaseTestCase() {
-		if(!System.getProperties().contains(OPENSHIFT_POSTGRESQL_DB_HOST)){
-			System.getProperties().setProperty(OPENSHIFT_POSTGRESQL_DB_HOST, "localhost");
-		}
-		if(!System.getProperties().contains(OPENSHIFT_POSTGRESQL_DB_PORT)){
-			System.getProperties().setProperty(OPENSHIFT_POSTGRESQL_DB_PORT, "5432");
-		}
-	}
+    public BaseTestCase() {
+        if (!System.getProperties().contains(OPENSHIFT_POSTGRESQL_DB_HOST)) {
+            System.getProperties().setProperty(OPENSHIFT_POSTGRESQL_DB_HOST, DEFAULT_DB_HOST);
+        }
+        if (!System.getProperties().contains(OPENSHIFT_POSTGRESQL_DB_PORT)) {
+            System.getProperties().setProperty(OPENSHIFT_POSTGRESQL_DB_PORT, DEFAULT_DB_PORT);
+        }
+    }
 
-	@Autowired
-	private WebApplicationContext wac;
-	private MockMvc mockMvc;
-	
-	@Before
-	public void setup() {
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
-	
-	protected MockMvc getMockMvc(){
-		return mockMvc;
-	}
 
-	protected String getResponseBody(String path) throws Exception {
-        MvcResult result = getMockMvc().perform(MockMvcRequestBuilders.get(path)
+    protected MockMvc getMockMvc() {
+        return mockMvc;
+    }
+
+    protected String getResponseBody(String path) throws Exception {
+        MvcResult result = getMockMvc().perform(get(path)
                 .accept(APPLICATION_JSON)).
-                andExpect(MockMvcResultMatchers.status().isOk())
+                andExpect(status().isOk())
                 .andReturn();
         return result.getResponse().getContentAsString();
     }
-	
+
 }
